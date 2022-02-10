803b3e0d75b1494fb44288ff26f588d8f2306d5f
MaddyLobb <81719007+MaddyLobb@users.noreply.github.com>
2022-02-10 09:59:14 -0500

    test

--- dev/js/mobileNavScrollTo.js
+++ dev/js/mobileNavScrollTo.js
@@ -3,7 +3,7 @@ import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 
 gsap.registerPlugin(ScrollToPlugin);
 
-let idArray = ["#hero","#mobile-community","#mobile-shop", "#mobile-food"];
+let idArray = ["#hero","#mobile-community","#mobile-shop","#mobile-food"];
 
 export function mobileScrollPage(index){
     gsap.to(window, {duration: 2, scrollTo:idArray[index]});
